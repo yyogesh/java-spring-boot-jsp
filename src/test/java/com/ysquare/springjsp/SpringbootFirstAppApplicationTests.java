77b69986e3f0628effbe241b219292d684e5dbb4
yyogesh <yogeshggn1@gmail.com>
2021-08-24 18:03:43 +0530

    Java Spring boot jsp

---
+++ src/test/java/com/ysquare/springjsp/SpringbootFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysquare.springjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
